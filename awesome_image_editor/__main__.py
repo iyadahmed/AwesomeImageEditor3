cafb81c6d40c9f01d33d893905434072481e7824
Iyad Ahmed <iyadahmed430@gmail.com>
2023-03-01 22:29:42 +0200

    Set organization name and domain

--- awesome_image_editor/__main__.py
+++ awesome_image_editor/__main__.py
@@ -5,7 +5,11 @@ from PyQt6.QtWidgets import QApplication
 from awesome_image_editor.main_window import MainWindow
 
 app = QApplication(sys.argv)
+
+app.setOrganizationName("SideProject")
+app.setOrganizationDomain("side-project.com")
 app.setApplicationName("Awesome Image Editor")
+
 mainWindow = MainWindow()
 mainWindow.showMaximized()
 app.exec()
