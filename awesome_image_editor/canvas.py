b480cbc0dc66cfd299186b2cc84179bc0fbaaa1a
Iyad Ahmed <iyadahmed430@gmail.com>
2023-03-01 23:31:44 +0200

    Implement panning with middle mouse button

--- awesome_image_editor/canvas.py
+++ awesome_image_editor/canvas.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
-from PyQt6.QtGui import QImage, QPainter, QPaintEvent
+from PyQt6.QtCore import QPoint, Qt
+from PyQt6.QtGui import QImage, QMouseEvent, QPainter, QPaintEvent, QWheelEvent
 from PyQt6.QtWidgets import QWidget
 
 
@@ -22,10 +23,31 @@ class CanvasWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.layers: list[Layer] = []
+        self._offsetStartPos = QPoint()
+        self._deltaOffset = QPoint()
+        self._offset = QPoint()
 
     def paintEvent(self, event: QPaintEvent) -> None:
         painter = QPainter(self)
+        painter.save()
+        painter.translate(self._offset + self._deltaOffset)
         for layer in self.layers:
             painter.save()
             layer.draw(painter)
             painter.restore()
+        painter.restore()
+
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        if event.buttons() & Qt.MouseButton.MiddleButton:
+            self._offsetStartPos = event.pos()
+
+    def mouseMoveEvent(self, event: QMouseEvent) -> None:
+        if event.buttons() & Qt.MouseButton.MiddleButton:
+            self._deltaOffset = event.pos() - self._offsetStartPos
+            self.update()
+
+    def mouseReleaseEvent(self, event: QMouseEvent) -> None:
+        if event.button() == Qt.MouseButton.MiddleButton:
+            self._offset = self._deltaOffset + self._offset
+            self._deltaOffset = QPoint()
+            self.update()
