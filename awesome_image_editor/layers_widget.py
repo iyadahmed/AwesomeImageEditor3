7f4308e1892d0d744261e010d10efdfde5e4824a
Iyad Ahmed <iyadahmed430@gmail.com>
2023-04-09 05:32:44 +0200

    Add icon next to tree view title label

--- awesome_image_editor/layers_widget.py
+++ awesome_image_editor/layers_widget.py
@@ -1,7 +1,12 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QSizePolicy, QSpacerItem
-from PyQt6.QtCore import Qt
+from pathlib import Path
+
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QSizePolicy, QSpacerItem, QHBoxLayout
+from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtGui import QIcon
 
 from awesome_image_editor.tree_view import LayersTreeView
+
+ICON_LAYERS = QIcon((Path(__file__).parent / "icons/layers/layers_dialog.svg").as_posix())
 
 
 class LayersWidget(QWidget):
@@ -10,6 +15,14 @@ class LayersWidget(QWidget):
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        label = QLabel("Layers")
-        layout.addWidget(label, stretch=0, alignment=Qt.AlignmentFlag.AlignTop)
+        titleLayout = QHBoxLayout()
+        titleLayout.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        titleIconLabel = QLabel()
+        titleIconLabel.setPixmap(ICON_LAYERS.pixmap(QSize(24, 24)))
+        titleLayout.addWidget(titleIconLabel)
+        titleLabel = QLabel("Layers")
+        titleLayout.addWidget(titleLabel)
+
+        layout.addLayout(titleLayout)
+        # layout.addWidget(label, stretch=0, alignment=Qt.AlignmentFlag.AlignTop)
         layout.addWidget(tree_view, stretch=1)
